6f4000cecbe05db9582cdc725f04ebb76fb23e2d
Suyash Kamath <suyashkamath9@gmail.com>
2025-11-08 21:33:55 +0530

    Leap year code corrected

--- 02_conditionals/9_leap_year_checker.py
+++ 02_conditionals/9_leap_year_checker.py
@@ -1,9 +1,9 @@
 # Problem: Determine if a year is a leap year. (Leap years are divisible by 4, but not by 100 unless also divisible by 400).
 
 
-year = 2023
+year = 2000
 
-if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-    print( year, " is a leap year")
+if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+    print(year, "is a leap year")
 else:
     print(year, "is NOT a leap year")
