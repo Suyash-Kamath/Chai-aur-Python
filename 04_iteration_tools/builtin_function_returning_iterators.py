59d70273a26df2ba9ed265ca1136afd77441167b
Suyash Kamath <suyash.kamath@probusinsurance.com>
2025-11-18 17:14:45 +0530

    built in function returning iterators

--- 04_iteration_tools/builtin_function_returning_iterators.py
+++ 04_iteration_tools/builtin_function_returning_iterators.py
@@ -1,3 +1,17 @@
 it = map(lambda x: x*2, [1, 2, 3])
 for x in it:
     print(x)
+
+print(list(it))
+print(list(it))
+
+
+"""
+Important Behavior of map():
+
+map() is lazy → it does NOT compute all values at once.
+
+The values are only created when iterating.
+
+Once exhausted, the iterator cannot be reset or reused.
+"""
